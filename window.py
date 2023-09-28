7c3569868df72cb08bd393eebf53334728277a7a
VEGAMETA <viperigor12@gmail.com>
2023-09-28 17:53:08 +0300

    c:; cd 'c:\Users\VEGAMETA\Documents\PythonProjects\FILE_TRANSFERER'; & 'c:\Users\VEGAMETA\Documents\PythonProjects\FILE_TRANSFERER\venv\Scripts\python.exe' 'c:\Users\VEGAMETA\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '63382' '--' 'main.py'

--- window.py
+++ window.py
@@ -1,3 +1,4 @@
+import re
 import time
 import asyncio
 import network
@@ -9,38 +10,41 @@ import PySimpleGUI as sg
 class FileTransfer(sg.Window):
     def __init__(self):
         self.selected_files = []
-        self.available_ips = []
+        self.available_ips = self.get_ips()
 
         layout = [
             [
-                sg.Text("Files", size=(32, 1)),
-                sg.Text("PC ip", size=(20, 1)),
-                sg.Checkbox("Get files", change_submits=True, key="Receive"),
-            ],
-            [
-                sg.Listbox(
-                    values=self.selected_files,
-                    size=(36, 6),
-                    key="files",
-                    no_scrollbar=True,
-                ),
-                sg.Listbox(
-                    values=self.available_ips,
-                    size=(36, 6),
-                    key="ips",
-                    no_scrollbar=True,
-                ),
-            ],
-            [
-                sg.Button("Add file", size=(14, 1)),
-                sg.Button("Del file", size=(14, 1)),
-                sg.Button("Send", size=(32, 1)),
+                [
+                    sg.Text("Files", size=(32, 1)),
+                    sg.Text("PC ip", size=(20, 1)),
+                    sg.Checkbox("Get files", change_submits=True, key="Receive"),
+                ],
+                [
+                    sg.Listbox(
+                        values=self.selected_files,
+                        size=(36, 6),
+                        key="files",
+                        no_scrollbar=True,
+                    ),
+                    sg.Listbox(
+                        values=[file.get("name") for file in self.available_ips],
+                        size=(36, 6),
+                        key="ips",
+                        no_scrollbar=True,
+                    ),
+                ],
+                [
+                    sg.Button("Add file", size=(14, 1)),
+                    sg.Button("Del file", size=(14, 1)),
+                    sg.Button("Add ip", size=(15, 1)),
+                    sg.Button("Del ip", size=(14, 1)),
+                ],
+                sg.Button("Send", size=(64, 3)),
             ],
         ]
         super().__init__("File transfer", layout, finalize=True)
         self.client_server = network.ClientServer()
         self.server_thread = None
-        threading.Thread(target=self.update_ips, daemon=True).start()
         self.main_loop()
 
     def main_loop(self):
@@ -69,6 +73,49 @@ class FileTransfer(sg.Window):
                         self.selected_files.remove(file)
                         break
                 self["files"].update(values=[file.name for file in self.selected_files])
+
+            elif event == "Add ip":
+                layout = [
+                    [sg.Text("Name:\t\t"), sg.Input(key="-NAME-")],
+                    [sg.Text("IP Address:\t"), sg.Input(key="-IP-")],
+                    [sg.Button("Submit")],
+                ]
+                window = sg.Window("Enter Name and IP", layout)
+
+                while True:
+                    event, values = window.read()
+                    if event == sg.WINDOW_CLOSED:
+                        break
+                    elif event == "Submit":
+                        name = values["-NAME-"]
+                        ip = values["-IP-"]
+                        pattern = r"^(\d{1,3}\.){3}\d{1,3}$"
+                        if not name:
+                            sg.popup_error("Name hasn't been entered")
+                        if re.match(pattern, ip) is not None:
+                            self.available_ips.append({"name": name, "ip": ip})
+                            window.close()
+                            self["ips"].update(
+                                values=[host.get("name") for host in self.available_ips]
+                            )
+                            with open("ips.txt", "w") as f:
+                                for host in self.available_ips:
+                                    f.write(f"{host.get('ip')} {host.get('name')}\n")
+                            break
+                        else:
+                            sg.popup_error("Invalid IP")
+
+            elif event == "Del ip":
+                for ip in self.available_ips:
+                    if ip.get("name") == values.get("ips")[0]:
+                        self.available_ips.remove(ip)
+                        self["ips"].update(
+                            values=[host.get("name") for host in self.available_ips]
+                        )
+                        with open("ips.txt", "w") as f:
+                            for host in self.available_ips:
+                                f.write(f"{host.get('ip')} {host.get('name')}\n")
+                        break
 
             elif event == "Send":
                 if not values.get("ips"):
@@ -99,13 +146,10 @@ class FileTransfer(sg.Window):
                     self.server_thread = None
                     self.client_server.close_connection()
 
-    def update_ips(self):
-        while True:
-            time.sleep(1)
-            try:
-                self.available_ips = self.client_server.get_ips()
-                self["ips"].update(
-                    values=[host.get("name") for host in self.available_ips]
-                )
-            except sg.tk.TclError as e:
-                print(f"Error updating ips: {str(e)}")
+    def get_ips(self):
+        with open("ips.txt", "r") as f:
+            ips = [
+                {"ip": host[0], "name": host[1]}
+                for host in [host.split(" ", 1) for host in f.read().splitlines()]
+            ]
+            return ips
