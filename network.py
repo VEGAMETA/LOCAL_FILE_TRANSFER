794d5465fcc8a13f8a670b3118f9ba9d1a6e80ae
VEGAMETA <viperigor12@gmail.com>
2023-09-28 10:42:25 +0300

    init

---
+++ network.py
@@ -0,0 +1,85 @@
+import socket
+import subprocess
+import typing
+
+
+class ClientServer:
+    def __init__(self):
+        self.server: typing.Optional[socket.socket] = None
+        self.PORT = 23091
+        self.downloads_folder = "D:/Downloads/"
+
+    @staticmethod
+    def get_ips():
+        local_ips = []
+
+        try:
+            arp_result = subprocess.check_output(["arp", "-a"], universal_newlines=True)
+            lines = arp_result.split("\n")
+            for line in lines:
+                parts = line.split()
+                if (
+                    len(parts) >= 2
+                    and parts[0].startswith("192")
+                    and parts[0] not in ("192.168.1.255", "192.168.1.1")
+                ):
+                    ip_address = parts[0]
+                    local_ips.append(
+                        {"name": socket.gethostbyaddr(ip_address)[0], "ip": ip_address}
+                    )
+        except subprocess.CalledProcessError:
+            pass
+
+        return list(local_ips)
+
+    async def send_files(self, ip, selected_files):
+        if selected_files and ip:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((ip, self.PORT))
+
+            for file in selected_files:
+                client.send(file.name.encode("utf-8"))
+                with open(file, "rb") as f:
+                    file_data = f.read(1024)
+                    while file_data:
+                        client.send(file_data)
+                        file_data = f.read(1024)
+
+            client.close()
+
+    async def open_connection(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(("0.0.0.0", self.PORT))
+        print("Server started")
+        self.server.listen(5)
+
+        while True:
+            try:
+                client_socket, _ = self.server.accept()
+                filename = client_socket.recv(1024).decode("utf-8")
+                self.handle_file_transfer(client_socket, filename)
+            except (KeyboardInterrupt, OSError) as e:
+                print(f"Error handling file transfer: {str(e)}")
+                break
+
+    def handle_file_transfer(self, client_socket, filename):
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                return
+
+            filename = self.downloads_folder + filename
+
+            with open(filename, "wb") as file:
+                while data:
+                    file.write(data)
+                    data = client_socket.recv(1024)
+
+        except Exception as e:
+            print(f"Error handling file transfer: {str(e)}")
+
+    async def close_connection(self):
+        if self.server:
+            self.server.close()
+            self.server = None
+            print("Server closed")
---
+++ window.py
@@ -0,0 +1,112 @@
+import asyncio
+import network
+import pathlib
+import threading
+import PySimpleGUI as sg
+
+
+class FileTransferer(sg.Window):
+    def __init__(self):
+        self.selected_files = []
+        self.available_ips = []
+        layout = [
+            [
+                sg.Text("Files", size=(32, 1)),
+                sg.Text("PC ip", size=(20, 1)),
+                sg.Checkbox("Get files", change_submits=True, key="Receive"),
+            ],
+            [
+                sg.Listbox(
+                    values=self.selected_files,
+                    size=(36, 6),
+                    key="files",
+                    no_scrollbar=True,
+                ),
+                sg.Listbox(
+                    values=self.available_ips,
+                    size=(36, 6),
+                    key="ips",
+                    no_scrollbar=True,
+                ),
+            ],
+            [
+                sg.Button("Add file", size=(14, 1)),
+                sg.Button("Del file", size=(14, 1)),
+                sg.Button("Send", size=(32, 1)),
+            ],
+        ]
+        super().__init__("File transfer", layout, finalize=True)
+        self.client_server = network.ClientServer()
+        self.server_thread = None
+        asyncio.run(self.run())
+        self.close()
+
+    async def run(self):
+        await asyncio.gather(self.update_ips(), self.main_loop())
+
+    async def main_loop(self):
+        while True:
+            event, values = self.read()
+
+            if event == sg.WIN_CLOSED or event == "Cancel":
+                exit(0)
+
+            elif event == "Add file":
+                filename = pathlib.Path(
+                    sg.popup_get_file("Will not see this message", no_window=True)
+                )
+                if filename in self.selected_files:
+                    sg.popup_error("Filename in the list already")
+                    continue
+                self.selected_files.append(filename)
+                self["files"].update(values=[file.name for file in self.selected_files])
+
+            elif event == "Del file":
+                for file in self.selected_files:
+                    if file.name == values.get("files")[0]:
+                        self.selected_files.remove(file)
+                        break
+                self["files"].update(values=[file.name for file in self.selected_files])
+
+            elif event == "Send":
+                if not values.get("ips"):
+                    sg.popup_error("Ip hasn't been chosen")
+                    continue
+                elif not self.selected_files:
+                    sg.popup_error("Files hasn't been chosen")
+                    continue
+                else:
+                    for host in self.available_ips:
+                        if host.get("name") == values.get("ips")[0]:
+                            ip = host.get("ip")
+                            break
+                    else:
+                        continue
+                try:
+                    await self.client_server.send_files(ip, self.selected_files)
+                except Exception as e:
+                    sg.popup_error(f"Error sending files: {str(e)}")
+
+            elif event == "Receive":
+                if values.get("Receive"):
+                    self.server_thread = threading.Thread(
+                        target=asyncio.run, args=(self.client_server.open_connection(),)
+                    )
+                    self.server_thread.start()
+                else:
+                    self.server_thread = None
+                    await self.client_server.close_connection()
+
+    async def update_ips(self):
+        while True:
+            try:
+                self.available_ips = self.client_server.get_ips()
+                values = [
+                    f"{host.get('name')} - {host.get('ip')}"
+                    for host in self.available_ips
+                ]
+                self["ips"].update(values=values)
+                await asyncio.sleep(1)
+            except sg.tk.TclError as e:
+                print(f"Error updating ips: {str(e)}")
+                break
